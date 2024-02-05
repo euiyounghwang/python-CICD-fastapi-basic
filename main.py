1cc1967eaf8c0355c7a3dc66cbb43e954f70973d
euiyounghwang <marieuig@gmail.com>
2024-02-05 15:15:29 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from config.log_config import create_log
+
+logger = create_log()
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+''' http://localhost:7777/docs '''
+
+@app.get("/", tags=['API'],  
+         status_code=200,
+         description="Default GET API", 
+         summary="Return Json")
+async def info():
+    logger.info('root')
+    return {"name": "python-CICD-sample-fast-api", "version": "1.0.0"}
+
+
+@app.get("/test", tags=['API'],  
+         status_code=200,
+         description="Default GET Param API", 
+         summary="Return GET Param Json")
+async def root_with_arg(id):
+    logger.info('root_with_arg - {}'.format(id))
+    return {"message": "Hello World [{}]".format(id)}
+
+
+@app.get("/test/{id}", tags=['API'],  
+         status_code=200,
+         description="Default GET with Body API", 
+         summary="Return GET with Body Json")
+async def root_with_param(id):
+    logger.info('root_with_arg - {}'.format(id))
+    return {"message": "Hello World [{}]".format(id)}
+
+
+# router
+# app.include_router(es_search_controller.app, tags=["Search"], )
---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from main import app
+# from injector import es_client, Redis_Cache
+
+# In order to share fixtures across multiple test files, pytest suggests defining fixtures in a conftest.py
+
+@pytest.fixture(scope="class")
+def mock_client():
+    # app = FastAPI()
+    client = TestClient(app)
+    
+    return client
+
