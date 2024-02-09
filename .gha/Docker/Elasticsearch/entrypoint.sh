aa7ca9350025792a4668a9b9b4032f068a28cb3d
euiyounghwang <marieuig@gmail.com>
2024-02-08 22:40:51 -0600

    Update GHA with Elasticsearch Plugins  for Postgres, Redis, Elasticsearch

--- .gha/Docker/Elasticsearch/entrypoint.sh
+++ .gha/Docker/Elasticsearch/entrypoint.sh
@@ -28,78 +28,52 @@ if docker network ls | grep -q "${network_name}"; then
 fi
 docker network create elastic
 
-# docker run \
-#   --env "node.name=es1" \
-#   --env "cluster.name=docker-elasticsearch" \
-#   --env "cluster.initial_master_nodes=es1" \
-#   --env "discovery.seed_hosts=es1" \
-#   --env "cluster.routing.allocation.disk.threshold_enabled=false" \
-#   --env "bootstrap.memory_lock=true" \
-#   --env "ES_JAVA_OPTS=-Xms1g -Xmx1g" \
-#   --env "xpack.security.enabled=false" \
-#   --env "xpack.security.http.ssl.enabled=false" \
-#   --env "xpack.license.self_generated.type=basic" \
-#   --env "action.destructive_requires_name=false" \
-#   --env "http.port=9200" \
-#   --ulimit nofile=65536:65536 \
-#   --ulimit memlock=-1:-1 \
-#   --publish "9209:9200" \
-#   --network=elastic \
-#   --name="es1" \
-#   --detach \
-#   -v /elasticsearch/plugins:/usr/share/elasticsearch/plugins \
-#   docker.elastic.co/elasticsearch/elasticsearch:${STACK_VERSION}
-
-# docker exec -u root es1 /usr/share/elasticsearch/bin/elasticsearch-plugin install  --batch analysis-stempel analysis-ukrainian analysis-smartcn analysis-phonetic analysis-icu analysis-nori analysis-kuromoji
-
-docker build \
-  -f "$(dirname "$0")/Dockerfile" \
-  -t fn-bees-omnisearch:omni_es \
-  --target omni_es \
-  "$(dirname "$0")/."
-
-
-docker run --rm -it --name=es1 --network=elastic --publish 9209:9200 --expose 9200 \
+docker run \
+  --env "node.name=es1" \
+  --env "cluster.name=docker-elasticsearch" \
+  --env "cluster.initial_master_nodes=es1" \
+  --env "discovery.seed_hosts=es1" \
+  --env "cluster.routing.allocation.disk.threshold_enabled=false" \
+  --env "bootstrap.memory_lock=true" \
+  --env "ES_JAVA_OPTS=-Xms1g -Xmx1g" \
+  --env "xpack.security.enabled=false" \
+  --env "xpack.security.http.ssl.enabled=false" \
+  --env "xpack.license.self_generated.type=basic" \
+  --env "action.destructive_requires_name=false" \
+  --env "http.port=9200" \
   --ulimit nofile=65536:65536 \
   --ulimit memlock=-1:-1 \
-  -e node.name=es1 \
-  -e cluster.initial_master_nodes=es1 \
-  -e discovery.seed_hosts=es1 \
-  -e discovery.type=single-node \
-  -e http.port=9200 \
-  -e http.cors.enabled=true \
-  -e bootstrap.memory_lock=true \
-  -e http.cors.allow-origin=* \
-  -e http.cors.allow-headers=X-Requested-With,X-Auth-Token,Content-Type,Content-Length,Authorization \
-  -e http.cors.allow-credentials=true \
-  -e xpack.security.enabled=false \
-  -e bootstrap.system_call_filter=false \
-  -e ES_JAVA_OPTS="-Xms1g -Xmx1g" \
+  --publish "9209:9200" \
+  --network=elastic \
+  --name="es1" \
+  --detach \
   -v /elasticsearch/plugins:/usr/share/elasticsearch/plugins \
-  fn-bees-omnisearch:omni_es
+  docker.elastic.co/elasticsearch/elasticsearch:${STACK_VERSION}
+
+docker exec -u root es1 /usr/share/elasticsearch/bin/elasticsearch-plugin install --batch analysis-stempel analysis-ukrainian analysis-smartcn analysis-phonetic analysis-icu analysis-nori analysis-kuromoji
 
 
-# docker restart es1
+docker restart es1
 
-# set +e
-# sleep 10
-# docker run \
-#   --network elastic \
-#   --rm \
-#   appropriate/curl \
-#   --max-time 10 \
-#   --retry 5 \
-#   --retry-delay 5  \
-#   --retry-connrefused \
-#   --show-error \
-#   --silent \
-#   http://es1:9200/_cat/plugins
+set +e
+sleep 10
+docker run \
+  --network elastic \
+  --rm \
+  appropriate/curl \
+  --max-time 10 \
+  --retry 5 \
+  --retry-delay 5  \
+  --retry-connrefused \
+  --show-error \
+  --silent \
+  http://es1:9200/_cat/plugins
 
-# exit_status=$?
-# if [ $exit_status -eq 0 ]; then
-#   echo "Elasticsearch up and running"
-# else
-#   docker logs es1
-#   echo -e "\033[31;1mERROR:\033[0m The command failed with exit status: $exit_status\033[0m"
-#   exit 1
-# fi
+exit_status=$?
+if [ $exit_status -eq 0 ]; then
+  echo "Elasticsearch up and running"
+else
+  docker logs es1
+  echo -e "\033[31;1mERROR:\033[0m The command failed with exit status: $exit_status\033[0m"
+  exit 1
+fi
