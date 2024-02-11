44925f9250a66658c26b181cea326d9025001a8c
euiyounghwang <marieuig@gmail.com>
2024-02-10 22:34:59 -0600

    Add Shell scripts for Elasticsearch in Docker

--- docker-run-entrypoints.sh
+++ docker-run-entrypoints.sh
@@ -11,6 +11,6 @@ cd /app/FN-Basic-Services
 
 # --
 # Waitng for ES
-# ./wait_for_es.sh $ES_HOST
+./wait_for_es.sh $ES_HOST
 
 poetry run uvicorn main:app --host=0.0.0.0 --port=5555 --workers 4
--- service_start_run.sh
+++ service_start_run.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 set -e
 
-# Activate virtualenv && run serivce
+source read_config.sh
+# --
+# Call this function from './DevOps_Shell/read_config.yaml.sh' to get ES_HOST value in config.yaml file
+get_value_from_yaml
+# --
+
+export PYTHONDONTWRITEBYTECODE=1
 
 # cd /Users/euiyoung.hwang/ES/Python_Workspace/python-django
 SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cd $SCRIPTDIR
 source .venv/bin/activate
+
+# --
+# Waitng for ES
+./wait_for_es.sh $ES_HOST
 
 uvicorn main:app --reload --host=0.0.0.0 --port=5555 --workers 4
