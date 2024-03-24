918420055b30b4239a54c1581192669d16c0bf38
Euiyoung Hwang <euiyoung.hwang@gxo.com>
2024-03-23 20:15:58 -0500

    Update sh with window os

--- service_start_run.sh
+++ service_start_run.sh
@@ -13,9 +13,15 @@ export PYTHONDONTWRITEBYTECODE=1
 SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cd $SCRIPTDIR
+
+VENV=".venv"
+
 # Python 3.11.7 with Window
-source .venv/Scripts/activate
-# source .venv/bin/activate
+if [ -d "$VENV/bin" ]; then
+    source $VENV/bin/activate
+else
+    source $VENV/Scripts/activate
+fi
 
 # --
 # Waiting for ES
